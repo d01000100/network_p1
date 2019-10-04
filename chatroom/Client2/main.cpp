b7568feabf61600f80f9dbc4e69c4aeb41b220e3
Dzuniga <dzuniga@muuwho.com>
2019-10-04 15:47:10 -0400

    Integration of Serializer & ProtocolManager

--- chatroom/Client2/main.cpp
+++ chatroom/Client2/main.cpp
@@ -1,12 +1,16 @@
 #define WIN32_LEAN_AND_MEAN			// Strip rarely used calls
 
+// Include our serializer and protocol
+#include <ProtocolManager.h>
 #include <Windows.h>
 #include <WinSock2.h>
 #include <WS2tcpip.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <conio.h>
 #include <iostream>
 #include <string>
+#include <vector>
 
 // Need to link with Ws2_32.lib
 #pragma comment (lib, "Ws2_32.lib")
@@ -18,6 +22,7 @@ int main(int argc, char** argv)
 {
 	WSADATA wsaData;
 	int iResult;
+	std::vector<std::string> receivedMessages;
 
 	// Initialize Winsock
 	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
@@ -90,15 +95,20 @@ int main(int argc, char** argv)
 		WSACleanup();
 		return 1;
 	}
-	printf("Successfully connected to the server on socket %d!\n", connectSocket);
+	printf("Successfully connected to the server on socket %d!\n", (int)connectSocket);
 
 	// Make user login
 	std::string client_name;
 	std::cout << "Input username:\t";
 	std::getline(std::cin, client_name);
 
+	LoginMessage loginMessage;
+	loginMessage.client_name = client_name;
+	SendBuffer* theBuffer = new SendBuffer();
+	*theBuffer = writeMessage(&loginMessage);
+
 	printf("Logging in to the server...\n");
-	iResult = send(connectSocket, client_name.c_str(), client_name.length(), 0);
+	iResult = send(connectSocket, (char*)theBuffer->getBuffer(), theBuffer->getDataLength(), 0);
 	if (iResult == SOCKET_ERROR)
 	{
 		printf("send() failed with error: %d\n", WSAGetLastError());
@@ -108,32 +118,83 @@ int main(int argc, char** argv)
 	}
 	// printf("Bytes sent: %d\n", iResult);
 
+	system("cls");
 	std::cout << "Welcome " << client_name << "!!\n" << std::endl;
+	std::cout << "======================================\n" << std::endl;
+	std::cout << "Received Messages: \n\n\n\n" << std::endl;
+	printf("Your Message: ");
 
 	// #3 write & read
 	bool should_exit = false;
+	std::string message;
+	DWORD NonBlock = 1;
+
 	while (!should_exit)
 	{
-		//const char* buffer = "Hello server!";
-		//printf("Sending a packet to the server...\n");
-		//iResult = send(connectSocket, client_name.c_str(), client_name.length(), 0);
-		//if (iResult == SOCKET_ERROR)
-		//{
-		//	printf("send() failed with error: %d\n", WSAGetLastError());
-		//	closesocket(connectSocket);
-		//	WSACleanup();
-		//	return 1;
-		//}
-		//printf("Bytes sent: %d\n", iResult);
+		if (_kbhit())
+		{
+			char ch = _getch();
+			message.push_back(ch);
+			system("cls");
+			std::cout << "Welcome " << client_name << "!!\n" << std::endl;
+			std::cout << "======================================\n" << std::endl;
+			std::cout << "Received Messages: \n" << std::endl;
+			for (int i = 0; i < receivedMessages.size(); i++)
+			{
+				printf("$ %s\n", receivedMessages[i].c_str());
+			}
+			std::cout << "\n\n\n" << std::endl;
+			printf("Your Message: %s", message.c_str());
 
-		// Receive a message from the server before quitting
-		char recvbuf[DEFAULT_BUFLEN];
-		int recvbuflen = DEFAULT_BUFLEN;
-		printf("Waiting to receive data from the server...\n");
-		iResult = recv(connectSocket, recvbuf, recvbuflen, 0);
+			if (ch == ESCAPE)
+			{
+				should_exit = true;
+			}
+			if (ch == RETURN)
+			{
+				theBuffer = checkMessage(message);
+				if (theBuffer == NULL) { continue; }
+				iResult = send(connectSocket, (char*)theBuffer->getBuffer(), theBuffer->getDataLength(), 0);
+				if (iResult == SOCKET_ERROR)
+				{
+					printf("send() failed with error: %d\n", WSAGetLastError());
+					closesocket(connectSocket);
+					WSACleanup();
+					return 1;
+				}
+			}
+		}
+
+		// Receive messages from the server
+		//char recvbuf[DEFAULT_BUFLEN];
+		//int recvbuflen = DEFAULT_BUFLEN;
+		iResult = ioctlsocket(connectSocket, FIONBIO, &NonBlock);
+		if (iResult == SOCKET_ERROR)
+		{
+			printf("ioctlsocket() failed with error %d\n", WSAGetLastError());
+			closesocket(connectSocket);
+			WSACleanup();
+			return 1;
+		}
+		//printf("Waiting to receive data from the server...\n");
+		RecieveBuffer rcvBuffer;
+		iResult = recv(connectSocket, (char*)rcvBuffer.getBuffer(), DEFAULT_BUFLEN, 0);
 		if (iResult > 0)
 		{
 			printf("Bytes received: %d\n", iResult);
+			rcvBuffer.setDataRecieved(iResult);
+			Message* MSG = readMessage(rcvBuffer);
+			if (MSG->type == RECIEVE)
+			{
+				RecieveMessage* rcvMSG = (RecieveMessage*)MSG;
+				std::string tempString;
+				tempString += (rcvMSG->room_name + "." + rcvMSG->sender_name + ":\t" + rcvMSG->message);
+				receivedMessages.push_back(tempString);
+				if(receivedMessages.size() > 3)
+				{ // erase the 1st element
+					receivedMessages.erase(receivedMessages.begin());
+				}
+			}
 		}
 		else if (iResult == 0)
 		{
@@ -141,11 +202,16 @@ int main(int argc, char** argv)
 		}
 		else
 		{
+			if (WSAGetLastError() == WSAEWOULDBLOCK) // currently no data available
+			{  
+				continue;
+			}
 			printf("recv failed with error: %d\n", WSAGetLastError());
 			closesocket(connectSocket);
 			WSACleanup();
 			return 1;
 		}
+
 	} // while (!should_exit)
 
 	// #4 close
