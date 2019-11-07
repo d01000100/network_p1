75cd5857cea74d919360d464aa5dacb5418f0475
Dzuniga <dave.zuna@gmail.com>
2019-11-07 12:24:09 -0500

    user creation and authentication with DB

--- chatroom/AuthServer/theMain.cpp
+++ chatroom/AuthServer/theMain.cpp
@@ -1,5 +1,3 @@
-#pragma warning(disable:4996)
-
 #include <cppconn/driver.h>
 #include <cppconn/exception.h>
 #include <cppconn/resultset.h>
@@ -24,6 +22,12 @@ $(SolutionDir)dev\include;
 $(SolutionDir)dev\lib\$(Platform)\$(Configuration);
 lib files, libcrypto.lib, libss.lib, mysqlcppconn.lib
 */
+#include <openssl\conf.h>
+#include <openssl\evp.h>
+#include <openssl\err.h>
+#include <openssl\sha.h>
+
+SHA256_CTX ctx;
 
 sql::Driver* driver;
 sql::Connection* con;
@@ -40,10 +44,75 @@ bool _should_close = false;
 
 AuthServer authServer;
 
+bool deleteByCreationDateInTableUser(time_t creationDate)
+{
+	std::stringstream prep;
+	prep.str(std::string());
+	prep << "delete from user where creation_date = FROM_UNIXTIME(" << creationDate << ");";
+	pstmt = con->prepareStatement(prep.str());
+	try
+	{
+		int result = pstmt->executeUpdate();
+		printf("%d row(s) affected.\n", result);
+	}
+	catch (sql::SQLException & exception)
+	{
+		std::cout << "# ERR: SQLException in " << __FILE__;
+		std::cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << std::endl;
+		std::cout << "# ERR: " << exception.what();
+		std::cout << " (MySQL error code: " << exception.getErrorCode();
+		std::cout << ", SQLState: " << exception.getSQLState() << ")" << std::endl;
+		return false;
+	}
+}
 
-void CreateUser(std::string email, std::string password)
+std::string createSalt(int saltSize)
+{
+	std::string salt;  /* Salt for the crypt() function  */
+	const char* salt_chars = "abcdefghijklmnopqrstuvwxyz" /* Range of character supported   */
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZ"  /* as a value for salt in crypt() */
+		"0123456789";
+	char password1[BUFSIZ], * buf;
+
+	/* Build salt */
+	srand(time(NULL));
+	//salt[0] = salt_chars[rand() % 62];
+	//salt[1] = salt_chars[rand() % 62];
+
+	for (int i = 0; i < saltSize; i++)
+	{
+		salt += salt_chars[rand() % 62];
+	}
+
+	std::cout << "aqui la sal xdxd:  " << salt << std::endl;
+
+	return salt;
+}
+
+std::string to_hex(unsigned char s) {
+	std::stringstream ss;
+	ss << std::hex << (int)s;
+	return ss.str();
+}
+
+std::string sha256(std::string line) {
+	unsigned char hash[SHA256_DIGEST_LENGTH];
+	SHA256_CTX sha256;
+	SHA256_Init(&sha256);
+	SHA256_Update(&sha256, line.c_str(), line.length());
+	SHA256_Final(hash, &sha256);
+
+	std::string output = "";
+	for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+		output += to_hex(hash[i]);
+	}
+	return output;
+}
+
+std::string createUser(std::string email, std::string password)
 {
 	sql::PreparedStatement* addUser;
+	sql::PreparedStatement* addWebAuth;
 	int UID = 1000;
 	struct tm* ptm;
 	time_t now;
@@ -55,7 +124,7 @@ void CreateUser(std::string email, std::string password)
 	try
 	{
 		int result = addUser->executeUpdate();
-		printf("%d row(s) affected.\n", result);
+		//printf("%d row(s) affected.\n", result);
 	}
 	catch (sql::SQLException & exception)
 	{
@@ -64,9 +133,10 @@ void CreateUser(std::string email, std::string password)
 		std::cout << "# ERR: " << exception.what();
 		std::cout << " (MySQL error code: " << exception.getErrorCode();
 		std::cout << ", SQLState: " << exception.getSQLState() << ")" << std::endl;
-		system("Pause");
+		//system("Pause");
+		return exception.what();
 	}
-	
+
 	prep.str(std::string());
 	prep << "select * from user where creation_date = FROM_UNIXTIME(" << now << ");";
 	pstmt = con->prepareStatement(prep.str());
@@ -76,18 +146,23 @@ void CreateUser(std::string email, std::string password)
 		while (rs->next())
 		{
 			UID = rs->getInt(1);
-			std::cout << "UID: " << UID << std::endl;
+			//std::cout << "UID: " << UID << std::endl;
 		}
 	}
 
+	std::string salt = createSalt(password.size());
+	std::string addedSalt = password + salt;
+	std::string hash = sha256(addedSalt);
+	//std::cout << "hash: " << hash << std::endl;
+
 	prep.str(std::string());
-	prep << "INSERT INTO web_auth (email, salt, hashed_password, userId) VALUES ("<< email<<","<<");";
-	// `id`,`email`, `salt`, `hashed_password`, `userId`
-	addUser = con->prepareStatement(prep.str());
+	prep << "INSERT INTO web_auth (email, salt, hashed_password, userId) VALUES ('" 
+		<< email << "','" << salt << "','" << hash << "','" << UID << "');";
+	addWebAuth = con->prepareStatement(prep.str());
 	try
 	{
-		int result = addUser->executeUpdate();
-		printf("%d row(s) affected.\n", result);
+		int result = addWebAuth->executeUpdate();
+		//printf("%d row(s) affected.\n", result);
 	}
 	catch (sql::SQLException & exception)
 	{
@@ -96,8 +171,76 @@ void CreateUser(std::string email, std::string password)
 		std::cout << "# ERR: " << exception.what();
 		std::cout << " (MySQL error code: " << exception.getErrorCode();
 		std::cout << ", SQLState: " << exception.getSQLState() << ")" << std::endl;
-		system("Pause");
+		//system("Pause");
+		deleteByCreationDateInTableUser(now);
+		return exception.what();
 	}
+	return ("succesful creation of user");
+}
+
+std::string authenticateUser(std::string email, std::string password)
+{
+	sql::PreparedStatement* addUser;
+	sql::PreparedStatement* addWebAuth;
+	std::stringstream prep;
+
+	// web_auth thingies
+	int webID = 1000;
+	std::string userMail;
+	std::string salt;
+	std::string hashedPassword;
+	int UID = 1000;
+	
+	prep.str(std::string());
+	prep << "select * from web_auth where email = '" << email << "';";
+	pstmt = con->prepareStatement(prep.str());
+	rs = pstmt->executeQuery();
+	if (rs->rowsCount() > 0)
+	{
+		while (rs->next())
+		{
+			webID = rs->getInt(1);
+			userMail = rs->getString(2);
+			salt = rs->getString(3);
+			hashedPassword = rs->getString(4);
+			UID = rs->getInt(5);
+		}
+	}
+	else
+	{
+		return "ERROR: account not found";
+	}
+
+	std::string try1 = password + salt;
+	std::string hash = sha256(try1);
+	// std::cout << "hash: " << hash << std::endl;
+
+	if (hashedPassword != hash)
+	{
+		return "Authentication failed, wrong password";
+	}
+
+	time_t now;
+	time(&now);
+	prep.str(std::string());
+	prep << "UPDATE user SET last_login=FROM_UNIXTIME(" << now << ") WHERE id = "<<UID<<";";
+	// "UPDATE MyGuests SET lastname='Doe' WHERE id=2";
+	addUser = con->prepareStatement(prep.str());
+	try
+	{
+		int result = addUser->executeUpdate();
+		//printf("%d row(s) affected.\n", result);
+	}
+	catch (sql::SQLException & exception)
+	{
+		std::cout << "# ERR: SQLException in " << __FILE__;
+		std::cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << std::endl;
+		std::cout << "# ERR: " << exception.what();
+		std::cout << " (MySQL error code: " << exception.getErrorCode();
+		std::cout << ", SQLState: " << exception.getSQLState() << ")" << std::endl;
+	}
+
+	return "Succesful login";
 }
 
 void key_listen() {
@@ -114,6 +257,27 @@ void key_listen() {
 
 int main(int argc, char** argv)
 {
+	SHA256_Init(&ctx);
+
+	try
+	{
+		driver = get_driver_instance();
+		con = driver->connect(server, username, password);
+		printf("Successfully connected to our database!\n");
+
+		con->setSchema(schema);
+		printf("Successfully set our schema!\n");
+	}
+	catch (sql::SQLException & exception)
+	{
+		std::cout << "# ERR: SQLException in " << __FILE__;
+		std::cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << std::endl;
+		std::cout << "# ERR: " << exception.what();
+		std::cout << " (MySQL error code: " << exception.getErrorCode();
+		std::cout << ", SQLState: " << exception.getSQLState() << ")" << std::endl;
+		system("Pause");
+		return 1;
+	}
 
 	if (!authServer.init()) { return 1; }
 
